397d655acdac3c190400bd1cfa3c5b7c6fe7a1e5
Adam Vícha <adamvicha2005@gmail.com>
2025-10-16 17:03:25 +0200

    #4 kk

--- IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21e6dc80d3b200b2efee4832af2725726d1d6be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b3278ad2f33b27f3d410c04c0efd9dae4ebcb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
