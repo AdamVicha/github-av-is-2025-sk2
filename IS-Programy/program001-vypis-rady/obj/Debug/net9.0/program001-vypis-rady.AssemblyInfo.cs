2f21cf6151e928e5d452bd70baedd73e7fb56262
Adam Vícha <adamvicha2005@gmail.com>
2025-10-23 10:50:40 +0200

    #5 Součet cifer

--- IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b3278ad2f33b27f3d410c04c0efd9dae4ebcb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397d655acdac3c190400bd1cfa3c5b7c6fe7a1e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
